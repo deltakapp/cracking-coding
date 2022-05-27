a069e2041bc716b2734f4a13cd10039771e6e396
Delta Kapp <deltakapp@gmail.com>
2022-05-26 22:08:23 -0700

    started rust solution 1.2 check_permutations

---
+++ rust/chapter-1/src/check_permutation.rs
@@ -0,0 +1,16 @@
+// Problem: given two strings, write a method to check if one is a permutation
+// of the other
+
+// Solution
+pub fn main(string_1: String, string_2: String) -> bool {
+    let chars_1 = string_1.chars();
+    let chars_2 = string_2.chars();
+
+    for char in chars_1 {
+        // count number of each char
+    }
+
+    // same for chars_2
+
+    //compare tallies
+}
